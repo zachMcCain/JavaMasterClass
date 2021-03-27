0771bde5a226daa9a19015cc11700cb1ff7f46a1
Zachary McCain <zacmmcca@gmail.com>
2021-03-27 09:21:07 -0500

    Messing with variable declarations.

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -2,5 +2,8 @@ public class Hello {
 
   public static void main(String[] args) {
     System.out.println("Hello Zach");
+
+    int myFirstNumber = 10 + 5;
+    System.out.println(myFirstNumber);
   }
 }
