12524097c73bbb5175b5778c76d55c730de8f40d
Zachary McCain <zacmmcca@gmail.com>
2021-03-27 10:08:32 -0500

    Working with expressions.

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -3,7 +3,12 @@ public class Hello {
   public static void main(String[] args) {
     System.out.println("Hello Zach");
 
-    int myFirstNumber = 10 + 5;
-    System.out.println(myFirstNumber);
+    int myFirstNumber = (10 + 5) + (2 * 10);
+    int mySecondNumber = 12;
+    int myThirdNumber = myFirstNumber * 2;
+    int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+    System.out.println(myTotal);
+    int myLastOne = 1000 - myTotal;
+    System.out.println(myLastOne);
   }
 }
