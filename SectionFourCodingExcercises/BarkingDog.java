c0d05e44b87e48a6603c1a2acd60d6b314afc617
Zachary McCain <zacmmcca@gmail.com>
2021-04-19 14:20:01 -0500

    Implemented BarkingDog Class
    Implemented for excercise 3 of the Java Master Class section 4.

---
+++ SectionFourCodingExcercises/BarkingDog.java
@@ -0,0 +1,12 @@
+public class BarkingDog {
+  public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+    if (hourOfDay < 0 || hourOfDay > 23) {
+      return false;
+    }
+    if ((hourOfDay < 8 || hourOfDay > 22) && barking) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
