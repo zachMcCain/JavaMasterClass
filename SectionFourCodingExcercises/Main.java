ddfbef8d2c2d96cb72d5caa05a46dce3f0fa38b8
Zachary McCain <zacmmcca@gmail.com>
2021-04-19 13:56:08 -0500

    Implemented SpeedConverter class with Main for testing.
    Fulfilled the requirements of the first coding excercise in the Java Master Class in section four.

---
+++ SectionFourCodingExcercises/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Main is running for section four.");
+    SpeedConverter.printConversion(1.5);
+    SpeedConverter.printConversion(10.25);
+    SpeedConverter.printConversion(-5.6);
+    SpeedConverter.printConversion(25.42);
+    SpeedConverter.printConversion(75.114);
+  }
+}
