04e6f38ac22b5b24fb1b78e3747577ae400f0697
Zachary McCain <zacmmcca@gmail.com>
2021-04-19 14:12:39 -0500

    Implemented MegaByteConverter class
    Implemented for excercise 2 of the Java Master Class section four.

--- SectionFourCodingExcercises/Main.java
+++ SectionFourCodingExcercises/Main.java
@@ -1,10 +1,7 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Main is running for section four.");
-    SpeedConverter.printConversion(1.5);
-    SpeedConverter.printConversion(10.25);
-    SpeedConverter.printConversion(-5.6);
-    SpeedConverter.printConversion(25.42);
-    SpeedConverter.printConversion(75.114);
+    MegaBytesConverter.printMegaBytesAndKiloBytes(2500);
+    MegaBytesConverter.printMegaBytesAndKiloBytes(-1024);
+    MegaBytesConverter.printMegaBytesAndKiloBytes(5000);
   }
 }
